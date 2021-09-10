cd28dda8207acdb9a8529bba797619ee437561a4
Pavlo Omelianchuk <pavlotvingo@gmail.com>
2021-09-10 12:28:41 +0100

    resolve GraphQL query for Bio page

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/Biography/Biography-section.jsx
+++ src/components/Biography/Biography-section.jsx
@@ -7,27 +7,36 @@ import { graphql, useStaticQuery } from 'gatsby';
 export default function Bio() {
   const pageQuery = useStaticQuery(graphql`
     query {
-      markdownRemark(id: { eq: "2b19a414-619e-5118-ba6e-d1b2c1a81111" }) {
-        id
-        frontmatter {
-          title
-          slug
-          excerpt
-          date
-          bioImage {
-            name
-            childImageSharp {
-              gatsbyImageData(layout: CONSTRAINED, formats: WEBP)
+      allMarkdownRemark(filter: { id: { eq: "2b19a414-619e-5118-ba6e-d1b2c1a81111" } }) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              slug
+              excerpt
+              date
+              bioImage {
+                name
+                childImageSharp {
+                  gatsbyImageData(layout: CONSTRAINED, formats: WEBP)
+                }
+              }
             }
+            html
           }
         }
-        html
       }
     }
   `);
-  const {
-    markdownRemark: { frontmatter, html },
-  } = pageQuery;
+  const node = pageQuery.allMarkdownRemark.edges[0].node;
+  console.log(JSON.stringify(node));
+  const json = JSON.stringify(node, null, 2);
+
+
+
+  const { frontmatter, html } = node;
+
   const image = getImage(frontmatter.bioImage);
 
   if (frontmatter !== null) {
